6643b11e5749f8840486fbed8c6db4474bc10ddf
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-27 15:27:01 -0400

    basic curses module screen setup successfully displaying a string

--- path-finder.py
+++ path-finder.py
@@ -14,3 +14,12 @@ maze = [
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
+
+# standard output screen -> will override terminal
+def main(stdscr):
+    stdscr.clear() # clear the screen
+    stdscr.addstr(0,0, "Hello World!") # add a string
+    stdscr.refresh() # refresh
+    stdscr.getch() # get character -> input statement
+
+wrapper(main)
