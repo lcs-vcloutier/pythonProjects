2327e734e2c14d299c89b01d0198c8d74d2210f5
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-27 16:00:54 -0400

    added breadth first search algorithm

--- path-finder.py
+++ path-finder.py
@@ -31,12 +31,49 @@ def find_start(maze, start):
         for j, value in enumerate(row): # j is the column
             if value == start: 
                 return i, j
+    return None
 
 # find the best path (this is the algorithm)
 def find_path(maze, stdscr):
     start = "O"
     end = "X" 
+    start_pos = find_start(maze, start)
 
+    q = queue.Queue() # first in first out data structure
+    q.put((start_pos, [start_pos])) # keep track of current position as well as path to get to that node !
+    visited = set() # all of the positions we've visited
+
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos # break current pos down
+        if maze[row][col] == end:
+            return path
+        
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+            
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+        
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    if row > 0: # CHECK UP 
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze): # CHECK DOWN
+        neighbors.append((row + 1, col))
+    if col > 0: # CHECK LEFT 
+        neighbors.append((row, col -1))
+    if col + 1 < len(maze[0]): # CHECK RIGHT
+        neighbors.append((row, col + 1))
+
+    return neighbors
 
 
 
