3789c052ffd107b64969435e7c12474011269b7c
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-27 15:29:22 -0400

    created a color pairing for the string

--- path-finder.py
+++ path-finder.py
@@ -17,8 +17,10 @@ maze = [
 
 # standard output screen -> will override terminal
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK) # initializing a color pairing
+    blue_and_black = curses.color_pair(1) # referencing id 1 from above
     stdscr.clear() # clear the screen
-    stdscr.addstr(0,0, "Hello World!") # add a string
+    stdscr.addstr(5,5, "Hello World!", blue_and_black) # add a string
     stdscr.refresh() # refresh
     stdscr.getch() # get character -> input statement
 
