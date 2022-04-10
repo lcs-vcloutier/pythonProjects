9022a668c49d9677ee551acb76c648a28f047154
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-04-09 22:26:40 -0400

    finished bubble sort

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,6 +1,13 @@
-import curses
-from curses import wrapper
-import queue
 import time
 
-numbers = [3, 6, 0, -10, 8, 10 , 2, 1, 5, 13, 11, 9]
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        print(arr); time.sleep(1)
+        for j in range(0, n-i-1):
+             if arr[j] > arr[j + 1] :
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+ 
+bubbleSort(arr)
